f0244c0d8418004c0b3d4c2471e01efe720b48fe
RAHULSHELKEMS <Rahulshelkems@gmail.com>
2024-06-12 21:23:15 +0530

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+const { nextui } = require("@nextui-org/react");
+
+const config: Config = {
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				rubik: ["Rubik", "sans-serif"],
+			},
+			container: {
+				center: true,
+			},
+		},
+	},
+	darkMode: "class",
+	plugins: [nextui()],
+};
+export default config;
